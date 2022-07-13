e23cf8e9dc751bc1bade5cd293ca4450f5ad1e59
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-07-14 04:50:33 +0530

    ci: always update scripts from main branch
    * now fork user dont have to worry about updates
    * everything will be updated from our main repo except those which are only for rom bringup
    * its basically same as doing rom bringup commit on upstreamd repo everytime you start the build
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

---
+++ extras.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# to export flags before compilation
+# or you can also apply any patch like rm -rf xx/xx & git clone url xx/xx
+
+# export TZ=Asia/Kolkata
+# export SELINUX_IGNORE_NEVERALLOWS=true
