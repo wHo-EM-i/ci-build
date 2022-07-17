c1d457020d737dced3f1bc31b3092df34b81995d
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-07-17 21:19:14 +0530

    ci: flag export fix
    * also move java_options max heap size flag to extra.sh
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # Normal build steps
 . build/envsetup.sh
 lunch aospa_lavender-user
+. extras.sh
 export TZ=Asia/Kolkata
 ./rom-build.sh lavender -t user -v beta -j$(nproc --all)
--- extras.sh
+++ extras.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+. /tmp/ci/function && check_build_type
+
 # to export flags before compilation
 # or you can also apply any patch like rm -rf xx/xx & git clone url xx/xx
 
-# export TZ=Asia/Kolkata
+export TZ=Asia/Kolkata
 # export SELINUX_IGNORE_NEVERALLOWS=true
+case "$build_type" in
+		 R|r)
+		 export _JAVA_OPTIONS="-Xmx10g"
+     export JAVA_TOOL_OPTIONS="-Xmx10g"
+esac
