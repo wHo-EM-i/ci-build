471ab254958d12949a8e11a5a1b5b74aa962e7cb
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-07-18 23:02:10 +0530

    ci: Lets build arrow12.1_4.19 | R
    * goodix fix experimental build
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Normal build steps
 . build/envsetup.sh
-lunch aospa_lavender-user
+lunch arrow_lavender-userdebug
 . extras.sh
 export TZ=Asia/Kolkata
-./rom-build.sh lavender -t user -v beta -j$(nproc --all)
+make bacon -j$(nproc --all)
