2ff89353c0e3715d70f51e301d4755d51c147a0a
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-08-05 22:44:19 +0530

    ci: fluid update | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ lunch fluid_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export WITH_GAPPS=true
-#export TARGET_INCLUDE_GAPPS=true
-#export TARGET_GAPPS_ARCH=arm64
+export WITH_GAPPS=true
+export TARGET_INCLUDE_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 
 compile_plox () {
 make bacon -j$(nproc --all)
