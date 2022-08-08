a772010c1dc9aa9d424f7fcc9bb93149a5ed817c
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-08-09 02:04:02 +0530

    ci: build ArrowOS | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 # Normal build steps
 . build/envsetup.sh
-lunch dot_lavender-user
+lunch arrow_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-#export SELINUX_IGNORE_NEVERALLOWS=true
-export TARGET_SUPPORTS_BLUR=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export WITH_GAPPS=true
-export DOT_BUILD_TYPE := UNMINKAN_GAPPS
-export TARGET_INCLUDE_GAPPS=true
-export TARGET_GAPPS_ARCH=arm64
+export SELINUX_IGNORE_NEVERALLOWS=true
+#export TARGET_SUPPORTS_BLUR=true
+#export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ARROW_GAPPS=true
+#export TARGET_INCLUDE_GAPPS=true
+#export TARGET_GAPPS_ARCH=arm64
 
 compile_plox () {
 make bacon -j$(nproc --all)
